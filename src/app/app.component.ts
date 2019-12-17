5aea867fd6f240b929b013f534c080ce2bd85696
caiovc91 <caio.vc91@gmail.com>
2019-12-17 13:14:35 -0300

    8 files modified, 20 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { Hero } from '../hero';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Clovis';
-  title = 'Tour of Heroes'
+export class AppComponent {
+  title = 'Tour of Heroes';
 }
+
+
+/*
+Copyright 2017-2018 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
